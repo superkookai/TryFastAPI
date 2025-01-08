2bb046b2c377404f3dd1dd6e2bf887c0157d80ab
Weerawut <weerawut@live.com>
2025-01-08 17:15:18 +0700

    Initial Commit

---
+++ TryFastAPI/TryFastAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryFastAPIApp.swift
+//  TryFastAPI
+//
+//  Created by Weerawut Chaiyasomboon on 8/1/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct TryFastAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
